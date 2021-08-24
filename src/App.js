9ff572dc921bcd9db46e95100e77c8de94074b34
Oleksii83 <zaaytav@gmail.com>
2021-08-24 12:39:16 +0300

    task 2.2

--- src/App.js
+++ src/App.js
@@ -1,45 +1,20 @@
 import { Component } from 'react';
 import './App.css';
+import Form from './Component/PhonebookForm/PhonebookForm';
 // import { NIL as NIL_UUID } from 'uuid';
 
 class App extends Component {
-  state = {
-    contacts: [],
-    name: '',
-  };
+  state = {};
 
-  handleNameChange = e => {
-    console.log(e.currentTarget.value);
-    this.setState({ name: e.currentTarget.value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    console.log(this.state);
+  formSubmitHandler = data => {
+    console.log(data);
   };
 
   render() {
     return (
       <div className="Input-form">
         <h2>Phonebook</h2>
-        <form className="Input-container" onSubmit={this.handleSubmit}>
-          <h3 className="Input-name"> Name </h3>
-          <label>
-            <input
-              className="Input"
-              value={this.state.name}
-              onChange={this.handleNameChange}
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-              required
-            />
-          </label>
-          <button type="submit" className="input-btn">
-            Add cotact
-          </button>
-        </form>
+        <Form onSubmit={this.formSubmitHandler} />
       </div>
     );
   }
